0755b84dc62dce29856902249dfdda918973885d
Monles <iceueb@gmail.com>
2022-05-09 10:31:14 +0000

    first commit

---
+++ day15.py
@@ -0,0 +1,31 @@
+# day-15 Q54
+# import re
+
+# email = "john@google.com elise@python.com"
+# pattern = "\w+@(\w+).com"
+# ans = re.findall(pattern,email)
+# print(ans)
+
+# day-15 Q55
+# email = input().split()
+# ans = [word for word in email if word.isdigit()]
+# print(ans)
+
+# day-15 Q56
+# s = input()
+# u = s.encode('utf-8')
+# print(u)
+
+# day-15 Q59
+# n = int(input())
+# sum = 0
+# for i in range(1, n+1):
+#     sum += i/(i+1)
+# print(round(sum, 2))
+
+
+# def question_59(n):
+#     print(round(sum(map(lambda x: x/(x+1), range(1, n+1))), 2))
+
+
+# question_59(5)
---
+++ day16.py
@@ -0,0 +1,27 @@
+# day-16 Q60
+# def f(n):
+#     if n == 0:
+#         return 0
+#     return f(n-1) + 100
+
+
+# n = int(input())
+# print(f(n))
+
+n = int(input())
+def f(x): return f(x-1)+100 if x > 0 else 0
+
+
+print(f(n))
+
+
+# day-16 Q61
+
+
+# day-16 Q62
+
+
+# day-16 Q63
+
+
+# day-16 Q64
